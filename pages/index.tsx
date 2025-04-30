94e510e6c1c883042c1a7cb18f005125e2f25cbf
David Pastor <davidpasttordev@gmail.com>
2025-04-30 16:20:17 -0600

    scanner 7

--- pages/index.tsx
+++ pages/index.tsx
@@ -102,15 +102,7 @@ export default function HomePage() {
                 className="text-gray-400 hover:text-gray-600"
                 aria-label="Ver detalle"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path d="M9 5l7 7-7 7" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
+     
               </button>
             </li>
           ))}
