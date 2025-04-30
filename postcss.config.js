55ad1ff4a332f543faa7ea575452934e78fbf866
David Pastor <davidpasttordev@gmail.com>
2025-04-30 17:33:06 -0600

    postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,7 @@
 // postcss.config.js
-/** @type {import('postcss-load-config').Config} */
 module.exports = {
   plugins: {
-    // Aquí usamos el paquete @tailwindcss/postcss en vez de tailwindcss
     '@tailwindcss/postcss': {},
-    // Y autoprefixer tal cual
     autoprefixer: {},
   },
 };
-
