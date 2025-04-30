fd767cf67f83d56c5cfb738a2445bf69241c398b
David Pastor <davidpasttordev@gmail.com>
2025-04-30 15:43:47 -0600

    scanner 2

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,8 @@
 // postcss.config.js
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
-    plugins: [
-      // Llamamos a la función exportada por @tailwindcss/postcss
-      require('@tailwindcss/postcss')(),
-      // Llamamos a la función de autoprefixer
-      require('autoprefixer')(),
-    ],
-  };
-  
+  plugins: {
+    tailwindcss: {},      // ESTE es el plugin que Next espera
+    autoprefixer: {},
+  },
+};
